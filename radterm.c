11704b3da210a254d9e483d0a95a0052c2253f39
Dakota Boin <dakota@logiblock.com>
2015-07-27 18:44:37 -0700

    User input is now done using select for both user input and serial input from the device.

--- radterm.c
+++ radterm.c
@@ -65,62 +65,76 @@ static void take_ownership(const char *port) {
   #endif
 
   // int chown(const char *path, uid_t owner, gid_t group);
+  //@@ check error returned from chown
   if (chown(port, user, group) == -1) { 
     printf("Error (can't `chown`): %s\n Please run as admin\n", strerror(errno)); 
     exit(EXIT_FAILURE); 
   }
 }
 
-int userInputAvailable(void)  
-{
-  struct timeval tv;
-  fd_set fds;
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
-  FD_ZERO(&fds);
-  FD_SET(STDIN_FILENO, &fds);
-  return select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
-  //return (FD_ISSET(STDIN_FILENO, &fds));
-}
 
 // loop checks for input on terminal and serial port & passes data to parsers.
-void cheapstat_reader(driver_t *driver) 
+void inputLoop(int * fdList, int fdListLength) 
 {
   int8_t c;
   uint8_t b;
   struct timeval t;
   unsigned long long millisecondsSinceEpoch;
+  int fdListMax;
+  int result;
+  fd_set fds;
+
+  if(fdListLength == 0)
+  {
+    printf("no list length.\n");
+    return;
+  }
+
+
+  c         = 0;
+  fdListMax = 0;
+
+  // find largest file descriptor
+  for(int i = 0; i < fdListLength; i++)
+  {
+    fdListMax = (fdList[i] > fdListMax) ? fdList[i] : fdListMax;
+  }
 
   // remove terminal canonical mode for parsing ease.
   ttyCanonicalMode(1);
 
-  // loops until ^C
-  while (driver->fd != -1) 
+  result = 0;
+  while(result != -1)
   {
-    gettimeofday(&t, NULL);
-    millisecondsSinceEpoch =
-        (unsigned long long)(t.tv_sec)  * 1000 +
-        (unsigned long long)(t.tv_usec) / 1000;
+    FD_ZERO(&fds);
+    for(int i = 0; i < fdListLength; i++)
+      FD_SET(fdList[i], &fds);
+    
+    result = select(fdListMax+1, &fds, NULL, NULL, NULL);
 
-    c = 0;
-    if(serialHasChar(driver->fd))
+    if(FD_ISSET(fdList[0], &fds))
     {
-      while(serialHasChar(driver->fd))
+      read(fdList[0], &b, 1);
+      printf("%c", b);
+      if(driver.time == 1 && b == '\n')
       {
-        read(driver->fd, &b, 1);
-        printf("%c", b);
-        if(driver->time == 1 && b == '\n')
-          printf("%11llu: ", millisecondsSinceEpoch);
+        gettimeofday(&t, NULL);
+        millisecondsSinceEpoch =
+          (unsigned long long)(t.tv_sec)  * 1000 +
+          (unsigned long long)(t.tv_usec) / 1000;
+        printf("%11llu: ", millisecondsSinceEpoch);
       }
+      
     }
-
-    while(userInputAvailable())
+    else if(FD_ISSET(fdList[1], &fds))
     {
       c = fgetc(stdin);
       if(c != '\r')
-        write(driver->fd, &c, 1);
+        write(fdList[1], &c, 1);
     }
   }
+
+  printf("\nexit.\n");
 }
 
 /*
@@ -146,7 +160,12 @@ static void start(driver_t *driver) {
 #endif
 
 
-  cheapstat_reader(driver);
+  int fdList[2];
+  fdList[0] = driver->fd;
+  fdList[1] = STDIN_FILENO;
+
+  //@@ change input loop input.
+  inputLoop(fdList, 2);
 }
 
 //@@ add ability to run as background thread.
@@ -179,7 +198,7 @@ int main(int argc, char **argv) {
   driver.verbose  = 0;
   driver.time     = 0;
 
-  printf("CheapStat Driver v0.0.2\n");
+  printf("RaDtErM v0.0.3\n");
 
   // parse command line arguments.
   while((c = getopt(argc, argv, "i:g:b:p:vdt")) != EOF)
